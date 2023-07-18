a50e75ae75536a0e51548eb27aa9ae58d0203264
Faith Ofogba <ofogbaonome@gmail.com>
2023-07-18 20:11:55 +0100

    first commit

---
+++ app/head.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <title>spoutpayment</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="Web site created using create-react-app"
+      />
+      {/* <link rel="stylesheet" href="https://use.typekit.net/gnc1lsn.css" /> */}
+    </>
+  );
+}
