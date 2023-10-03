5227db1bfaf5eb5f517e4b2edb680f5e536c3d42
Faith Ofogba <ofogbaonome@gmail.com>
2023-10-03 15:03:52 +0100

    fixes merge conflicts

--- app/checkout/qr-code/QrCode.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function QrCode() {
-  return (
-    <div>QrCode</div>
-  )
-}
--- app/components/CustomDropdown.js
+++ app/components/CustomDropdown.js
@@ -1,18 +1,13 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const CustomDropdown = ({ options, label, name, setValue, register }) => {
+const CustomDropdown = ({ options, label }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
-  useEffect(() => {
-    register(name);
-    console.log("Is registered", register); // Register the dropdown input with useForm
-  }, [name, register]);
 
-  const handleOptionClick = (selectedValue) => {
-    setSelectedOption(selectedValue);
-    setValue(name, selectedValue);
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
     setIsOpen(false);
   };
 
@@ -29,7 +24,7 @@ const CustomDropdown = ({ options, label, name, setValue, register }) => {
             type="button"
             className="inline-flex justify-start w-full px-2.5 pt-6 pb-2.5 rounded text-xs text-gray-700 bg-white border border-gray-300 focus:outline-none"
           >
-            {selectedOption ? selectedOption : "Select an option"}
+            {selectedOption ? selectedOption.label : "Select an option"}
           </button>
         </span>
       </div>
@@ -44,7 +39,7 @@ const CustomDropdown = ({ options, label, name, setValue, register }) => {
             {options.map((option) => (
               <button
                 key={option.value}
-                onClick={() => handleOptionClick(option.value)}
+                onClick={() => handleOptionClick(option)}
                 className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 border-b"
                 role="menuitem"
               >
