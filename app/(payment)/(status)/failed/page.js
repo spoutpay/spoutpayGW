4a6e9bf0e42f850587fb2639cd1e9fd59b10eba4
Faith Ofogba <ofogbaonome@gmail.com>
2023-10-03 12:27:55 +0100

    UI cleanups

---
+++ app/(payment)/(status)/failed/page.js
@@ -0,0 +1,36 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Image from "next/image";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Failed() {
+  const router = useRouter();
+
+  useEffect(() => {
+    localStorage.removeItem("encryptedData");
+  });
+  return (
+    <div className="mt-4 flex justify-center items-center">
+      <div>
+        <div>
+          <Image
+            src="/failed.svg"
+            height="0"
+            width="0"
+            alt="success"
+            style={{ width: "250px", height: "250px" }}
+          />
+        </div>
+        <div className="mt-6 w-full">
+          <Button
+            text="Done"
+            variant="tulip"
+            onClick={() => router.push("/checkout")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
